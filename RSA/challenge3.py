29f2e1d91379e9844e36f3659ec60cded1799f68
Andrew Leonard <32071755+B00095125@users.noreply.github.com>
2019-03-21 19:43:50 +0000

    Add files via upload

--- RSA/challenge3.py
+++ RSA/challenge3.py
@@ -1,9 +1,7 @@
-## Commands to generate keys with openssl from commandline.. not part of this pythonj code.
-## openssl genrsa -out mykey.pem
-## openssl rsa -in mykey.pem -pubout > mykey.pub
-## -------------------------------------------------------------------------
+#Challenge 3#
+#Solved using crypto library#
+#Solution with help from https://stackoverflow.com/questions/39074253/extract-publickey-from-privatekey-input-using-python#
 
-## To run type python rsa.py from the commandline (assuming you've pythonh installed
 import binascii
 from Crypto.PublicKey import RSA
 
@@ -15,16 +13,20 @@ def string2int(my_str):
 def int2string(my_int):
     return binascii.unhexlify(format(my_int, "x").encode("utf-8")).decode("utf-8")
 
+#private key#
 pem_key = b'MIIEpQIBAAKCAQEAzGm6Ogrmlf4OSPDQ5nUIiDAJeR9wHdhce7m3NprE3QQqggdrK/itQlqfRxqTP8uFDzuvLCHkiBIkWXiB9uYx4nVP+y5Db0p8cA453Xc2wvpYO8m/6n3VFkAQyzIs7vSr77KqVyPvRzSn9OAjn9FEg1Z7TEmcbkfTo39S1urTBaTTvffwULIaEu/QWcVFnwjPAtVd0vwbgvTMIq09jiBQ+yi/AM8bqfPPG+2XVODk40f4ppX5IRngjjyR+2mXaK95hcv/EHNOSXkHGfvaJfLSbMtCKvVcPiajEZkQirq9PD/9VN8iAz30PoA5ok5NURhY8veU+rTFoZG3YHk7N6fhjwIDAQABAoIBAQDKgdvKcM4rvnssa9ao2TzQnrZj1m9eQeCtejk10XJCe0QZeXwFHeGXoOu2p29Ffjyd8MUD9bfPzhlQwgAPN9InxYytDRIliSdqY82Tx+zqkNUktiR5DJwz5Ng+VcEKIj7LwrbaiXEdm97gy8S/KbS0YNLZqvtcja/vg83vuMfCB9mz72vetpGhXQ9zRR4w89XjfEaIG280vPL3dmZ2I2YqlXpc6AggybdJpuJ/SNXhhKmRE28SrG8/yvy+NJeykgW5RXuDnBLJD5KlNANxl6ly1HKYTN6S7tP0BQDToqIapYjcKdxAsoOvUYd6hrdmqGgDbvI4XIZ4AMfQIjHr8s4RAoGBAOVAAW2pPC/45qNHYXHJ/vVHZD0UxE0tvsc2sHS5AT44Seb3hPwQQnSvE8y+tS2XioTDq1mYHE719V2KHJ5aciWkhuDeNn4FdSvMnz+0NawDwMSTDXyDvDlAA7PsA+8V65tDFDcABQPvKz5GK4bYbTIDFrgfgbibYtOrfnT3UcILAoGBAORDztBCPks5h4dVIkw52AYp/mNSex9sQsmkUqeFNMoz/sEgUJf7uRjNHP6R3iFB32WvVrj3e1ddTG5cHSc1DBQ8UuAxZzz14VFLvhYUvG+rHAXyDzqhNvZVjuBR5ErMD6bJ1r7HQ0ydceV15femeoBXEcnIcZ1FXjKeGDxltnUNAoGALNPCM75G7Z5/Auh/Tm/QMggeuq7n36uVRYEVKg3PB2qcUNSPpXZMeGKPvZaA+QRL6sAULnXG+02vB/ZsuC45adDtKuVoxGWuzry5WwyS/irRs96JYZKk6JDy6Gi7MDIaGwcX2dVgJa/LxeaUtk51s7TU6XYHuKBxx7AeDyMZUpcCgYEAvWF4o5ZiIn0vcVtzojRXgv2yPes/lVl3q932aV/95UjwMoDB/OZusiHyzU5uMb96Pd4UIE/LeDdC40jvMwky5VMLG1BBq/T/pDgoFB/OGwOms1QZyHXaqNNhP8ERm/Djh2hsD0o5DsaNqWeAjVAE0JfsfTIc+POFbI934hwtHb0CgYEAu0xdmmVf3uE0vrDoNA4cHSoPzupdFAXDv2G7NofJcOaNT9k6Zeut751jCN/bGlYmM3a0l/qBKmW/D/YP0NaLk3u1g5l8f3Rh+QwC3fCHcVAF/7ZFQJ5XhD90uXyXvv6qZFlD8Gq58vCmvg5HYyKnVIEhy25mDXrRyYVgnKEm9Ew='
+
+#decode the key and assign it the value of keypriv#
 key = b64decode(pem_key)
 keyPriv = RSA.importKey(key)
 
-#print(keyPriv.keydata)
+#Extract needed information from each aspect of the key, including n, e, d, p, q
 modulusN = keyPriv.n
 pubExpE = keyPriv.e
 priExpD = keyPriv.d
 primeP = keyPriv.p
 primeQ = keyPriv.q
+#print n, e and d
 print(modulusN)
 print(pubExpE)
 print(priExpD)
--- RSA/challenge4.py
+++ RSA/challenge4.py
@@ -1,8 +1,4 @@
-## Commands to generate keys with openssl from commandline.. not part of this pythonj code.
-## openssl genrsa -out mykey.pem
-## openssl rsa -in mykey.pem -pubout > mykey.pub
-## -------------------------------------------------------------------------
-
+#Challenge 4#
 import binascii
 from Crypto.PublicKey import RSA
 from base64 import b64decode
@@ -13,22 +9,22 @@ def string2int(my_str):
 def int2string(my_int):
     return binascii.unhexlify(format(my_int, "x").encode("utf-8")).decode("utf-8")
 
-public_key_filename = 'mykey2'
-rsa = M2Crypto.RSA.load_pub_key(pk)
+##The exact same logic is used in order to extrace necessary information in order to crack ciphertext#
 
 pem_key = b'MIIEogIBAAKCAQEAlVmF7X4kiOIn5VKe/TYXBzvg6guyUFQjCr5rNTN+AonEgO1kAVU76EY+8KhnQU81mO87+x1bqBGRmvM88xspXqjcif5AO9YEu7j4ssHuFk8yIr/gcGKwnzNF78oPST9UYxZi61dpIKcbh7dJNXd4fLSIu5OGfOKw0SuJ4qTgvcz+MdBUkYtlQdp5hnSIqpBPCKfd8kRAU4e+gR8vm4mhY5b8EiEvOaoUJC5KF/ykyfeFz3l4vWZcZn1R+MGNJ7bjDhhq1Dwaxew1hEPJGTj3gLzsifk4RYi8znCHGoNR0aK56st/NtWUYSvdbN7lULH26SgozR4FpRGgj06lbAXocQIDAQABAoIBAGn0pFZ0WrkeMY25SwB7gmTob028VJo2xtGVfHu291wXn2iUgpGLhb/pLjgQUYj9CBpjB5vFHUSkijdy/7e9emEkzam8zpdk4+DGfvJbMW10bC5JEJLLAWFAwAo5wRSHMHhH5o/uaK/4vkhPmUmsY7edYTAqnow1S5pqGB3KOYONOQf4IhwgqgsO368kEU6uqVCIeroXEb4Cb8vtqj45+s9FaxdbW+15fzcRyJU6EKZiewcNk1j3qVwcLmPnb3nVXgiKyfg8kkRXxrLrrY0MizMvc3IUFvr/BkvZNYtN/aKGv7MNAfkEoUf35QPz2rArThABG3VwwGzKEUE1ICQ63A0CgYEAxYxEM7z2PEi+3lN2tjKggBWxlh/RoP+yuwf5xEdunoxoDpeDU8op8k5sm1DYQcilhOJKwlXbC/V4bpkFE706ea0MuYVMV1CvWw1R2MNiD08tS2ej5k9QWw7B9rJe3rfc6cQemf4Wf2OU3DMVmXdX7hKZhTTbYKUsXntPYA16gtMCgYEAwYped4EA2Ed1Ti5+lndBEi0GCu8V3T9FlYtoLDQFYMElrPYV4r4tsABg1xTfMDtt+ige288chXWvZ798aQXxPlbkxgsuwpED3GHjX3giuQq+8yMYfWoX47ZbhVp+4q6JTofaEPSXGJPCOKIUwe92tVU4ltcFjnop80lD6t8K9SsCgYBJGYVnS8YmOpj+dg4Yj5VasCxdq/qLQ/MkSGhBHFmvq68ZrzE2gTmSYzjHJFxRw2iUSpR1YwXCYBwneNIrIruPqNxrjgrYyI+K9tvRhUM0/Qx6uagpojHO8CAQJKL8cTb6/cqBG0fB7dx4uyKD/mh4PUO3yf5fvd80Oxb9KR+rZwKBgBkudrIXz9NUINgmRoQvSgEiaIndp7ucsuINPGWb8yMunkdN0XUqB2nB8tepOIze/qUwuSsiXHSk5kiaaoaDpmrVBXkbFV9tXNI8zkeqquHmmwephlMzDZ6sKGZGawH1cZoNJPtVx58EAp45wHenP4vCZdWCbo+mJ3UnEDYsBRH3AoGAYu83TpvPqLP3T2ukzRHo+wC+vFhuxEuT8bA0M/MOyxDxuLyW+PmNSpnNY0CWHR0JHxLWnfyQcXp/KBmJf3JoWbblblufQkX6av9W6FyekgBYyv+1vrWf8+7Bj9JevlTWCoaiKFVYOsPcvRrqoXKg9OiCvChU6oxE9uuuXLD18TE='
 key = b64decode(pem_key)
 keyPriv = RSA.importKey(key)
 
-#print(keyPriv.keydata)
 modulusN = keyPriv.n
 pubExpE = keyPriv.e
 priExpD = keyPriv.d
 primeP = keyPriv.p
 primeQ = keyPriv.q
 
+#Ciphertext to be cracked
 ciphertext = 474862643754336865489984490773307542016161159436213530034995807183836312346778617047666360854948178434525541089212091928949344492697684657497682106740050084305554758259427768463395264318566101255923490595579348647860471822284428834756812967844672795316325109976652375135659724572710513755433401072885408968307124213606768098411795080747616961236626790699862671834311406129266854138764009952421206625693567227556664511584573464971029270576495696636132292906861410359486612705079004947333371264698887189359265840678094723729950785568382017843975809503403984016678927664449791524785943376314787680072596720311587221852
 
+#Once the necessary information has been extracted, the same function from first challenges can be used in order to break the cipher
 decrypted = pow(ciphertext, priExpD, modulusN)   ## decrypt
 plaintext = int2string(decrypted)
 print (plaintext)
